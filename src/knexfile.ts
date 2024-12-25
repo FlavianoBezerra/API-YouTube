401c41596db0c72ac55c4ba437047a269692599a
Flaviano Bezerra <flaviano202014@gmail.com>
2024-12-25 20:44:44 -0300

    feat: add SSL, connection pool, and path in the knex settings

--- src/knexfile.ts
+++ src/knexfile.ts
@@ -1,8 +1,24 @@
+require('ts-node/register');  // Registra o ts-node para rodar TypeScript
+import { config } from 'dotenv';
 import { Knex } from 'knex';
+
+config();
 
 const knexConfig: Knex.Config = {
   client: 'pg',
-  connection: process.env.DATABASE_URL,
+  connection: {
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false
+    }
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    directory: './dist/migrations'
+  }
 };
 
 export default knexConfig;
