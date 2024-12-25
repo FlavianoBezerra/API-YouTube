800dd3a982f341510d6b03c39711cca40852aa89
Flaviano Bezerra <flaviano202014@gmail.com>
2024-12-25 20:29:04 -0300

    feat: add typings and detailed logs to table creation process

--- src/migrations/20241218031425_create_users_table.ts
+++ src/migrations/20241218031425_create_users_table.ts
@@ -1,15 +1,19 @@
 import { Knex } from 'knex';
 
 export const up = (knex: Knex): Promise<void> => {
-  return knex.schema.createTable('users', (table) => {
+  return knex.schema.createTable('users', (table: Knex.CreateTableBuilder) => {
     table.increments('user_id').primary();
     table.string('name').notNullable();
     table.string('email').notNullable().unique();
     table.string('password').notNullable();
     table.timestamps(true, true);
+  }).then(() => {
+    console.log('Table created successfully');
   });
 };
 
 export const down = (knex: Knex): Promise<void> => {
-  return knex.schema.dropTableIfExists('users');
+  return knex.schema.dropTableIfExists('users').then(() => {
+    console.log('Table dropped successfully');
+  });
 };
--- src/migrations/20241218031538_create_videos_table.ts
+++ src/migrations/20241218031538_create_videos_table.ts
@@ -1,20 +1,22 @@
 import { Knex } from 'knex';
 
 export const up = (knex: Knex): Promise<void> => {
-  return knex.schema.createTable('videos', (table) => {
+  return knex.schema.createTable('videos', (table: Knex.CreateTableBuilder) => {
     table.increments('video_id').primary();
     table.integer('user_id').unsigned().notNullable();
     table.string('video_title').notNullable();
     table.string('video_description').notNullable();
     table.string('image_url').notNullable();
     table.timestamp('post_time').defaultTo(knex.fn.now());
-    
-    table.foreign('user_id').references('users.user_id').onDelete('CASCADE'); // Deleta os vídeos se o usuário for deletado
-
     table.timestamps(true, true);
+    table.foreign('user_id').references('users.user_id').onDelete('CASCADE'); // Deleta os vídeos se o usuário for deletado
+  }).then(() => {
+    console.log('Table created successfully');
   });
 };
 
 export const down = (knex: Knex): Promise<void> => {
-  return knex.schema.dropTableIfExists('videos');
+  return knex.schema.dropTableIfExists('videos').then(() => {
+    console.log('Table dropped successfully');
+  });
 };
