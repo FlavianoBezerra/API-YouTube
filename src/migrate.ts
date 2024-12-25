46b22b6bef55112d82bde714c7fd4f39bf9e14c5
Flaviano Bezerra <flaviano202014@gmail.com>
2024-12-25 20:46:25 -0300

    chore: update migration script with detailed logging and error handling

--- src/migrate.ts
+++ src/migrate.ts
@@ -1,24 +1,18 @@
 import knex from 'knex';
 import knexConfig from './knexfile';
 
-const db = knex(knexConfig);
+const configMigrate = knex(knexConfig);
 
-const runMigrations = async () => {
+async function runMigrations() {
   try {
-    console.log('Iniciando a aplicação das migrações...');
-    await db.migrate.latest();
-    console.log('Migrações aplicadas com sucesso!');
+    await configMigrate.migrate.latest();
+    console.log('Migrations completed!');
   } catch (error) {
-    if (error instanceof Error) {
-      console.error('Erro ao aplicar as migrações:', error.message);
-      console.error('Stack Trace:', error.stack);      
-    } else {
-      console.error('Erro desconhecido:', error);
-    }
+    console.error('Error running migrations', error);
   } finally {
-    await db.destroy();
-    console.log('Conexão com o banco fechada.');
+    configMigrate.destroy();
+    console.log('Connection closed');
   }
-};
+}
 
-export { db, runMigrations }; //npx ts-node migrate.ts, para exutar as migrações!
+runMigrations();
